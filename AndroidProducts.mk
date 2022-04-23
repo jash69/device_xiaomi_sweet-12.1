8665e2398a5561280e30fda22e5f1955684542f9
jash <jashvakharia@gmail.com>
2022-04-23 08:45:26 +0200

    sweet: Build Derpfest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_sweet.mk
+    $(LOCAL_DIR)/derp_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_sweet-eng \
-    arrow_sweet-userdebug \
-    arrow_sweet-user
+    derp_sweet-eng \
+    derp_sweet-userdebug \
+    derp_sweet-user
